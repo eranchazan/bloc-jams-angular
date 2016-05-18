806cd4227024f304151ba5f73a2b0c50a34da712
Eran Chazan <eranchazan@gmail.com>
2016-05-18 01:09:35 -0700

    services_1 checkpoint

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,11 @@
 (function() {
- 	function AlbumCtrl() {
+ 	 function AlbumCtrl(Fixtures) {
  		this.albumData = albumPicasso
+ 	    this.albumData = Fixtures.getAlbum();
  		// this.albumData.push(angular.copy(albumPicasso));
  	} 
 //album.albumData to access properties of album in DOM
  	angular
  	.module('blocJams')
- 	.controller('AlbumCtrl', AlbumCtrl);
+ 	.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
